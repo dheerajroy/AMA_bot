4dd2d39d6a43fb5cf5441a1182b0af7229c71e12
dheerajroy <dhiraajroy123@gmail.com>
2022-02-05 23:59:03 +0530

    Added comments to make the code readable

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import os
 
 def main():
     file = 'result.mp3'
-    result = summary(input('Enter Query: '), 2)
+    result = summary(input('Enter Query: '), 2) # Asks for user input and fetches the results
     print(result)
-    engine = gTTS(result, lang='en-us')
-    engine.save(file)
-    playsound(file)
-    os.remove(file)
+    engine = gTTS(result, lang='en-us') # gTTS class takes in result as text
+    engine.save(file) # The speech is saved to a file
+    playsound(file) # The speech is played
+    os.remove(file) # Speech file in removed after its been played
 
 if __name__ == '__main__':
-    main()
+    main() # Executed only when this particular file is running
