921f6d426e2e5538fd3fa56d2d908d73de086f1f
dheerajroy <dhiraajroy123@gmail.com>
2022-02-07 17:34:43 +0530

    Listening functionality added

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from wikipedia import summary
 from gtts import gTTS
 from playsound import playsound
+import speech_recognition as sr
 import os
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        try:
+            r.adjust_for_ambient_noise(source)
+            print('Listening...')
+            audio = r.listen(source)
+            print('Processing...')
+            text = r.recognize_google(audio)
+            print(f'You said: {text}')
+            return text
+        except Exception:
+            raise Exception('An Error Occured!') # Raises an error when there is a trouble in understanding the user
 
 def main():
     file = 'result.mp3'
-    result = summary(input('Enter Query: '), 2) # Asks for user input and fetches the results
+    result = summary(listen(), 2)
     print(result)
     engine = gTTS(result, lang='en-us') # gTTS class takes in result as text
     engine.save(file) # The speech is saved to a file
